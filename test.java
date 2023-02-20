0ee7f5445688a39bd293201049191e8a669265e7
NeilB18 <82923875+NeilB18@users.noreply.github.com>
2023-02-20 15:57:29 -0800

    Update test.java
    just some changes

--- test.java
+++ test.java
@@ -3,21 +3,22 @@ public class test{
         int[] array = {5,3,1,4,2};
         selectionSort(array);
         for (int i:array){
-            System.out.print(i);
+            System.out.print(i+" ");
         }
     }
 
     private static void selectionSort(int[] array) {
-        for (int i = 0 ; i<array.length-1 ; i++){
+        for (int i = 0 ; i<array.length-1;i++){
             int min = i;
-            for (int j = i+1; j<array.length; j++){
+            for (int j = i+1; j<array.length;j++){
                 if (array[min]>array[j]){
                     min = j;
                 }
             }
-            int temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
+
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
         }
 
 

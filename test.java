378758426d87d3bcc92a89f7246724701d186723
NeilB18 <82923875+NeilB18@users.noreply.github.com>
2023-02-21 09:51:35 -0800

    ...

--- test.java
+++ test.java
@@ -1,12 +1,35 @@
 public class test{
+    /**
+     * @param args
+     */
     public static void main(String[] args){
         int[] array = {5,3,1,4,2};
-        selectionSort(array);
+        //selectionSort(array);
+        insertionSort(array);
         for (int i:array){
             System.out.print(i+" ");
         }
     }
+    
+    /**
+     * @param array
+     */
+    private static void insertionSort(int[] array) {
+        for (int i = 1 ; i<array.length;i++){
+            int temp = array[i];
+            int j = i-1;
+            
+            while(j>0 && array[j]>temp) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
+        }
+    }
 
+    /**
+     * @param array
+     */
     private static void selectionSort(int[] array) {
         for (int i = 0 ; i<array.length-1;i++){
             int min = i;
@@ -20,7 +43,7 @@ public class test{
             array[min] = array[i];
             array[i] = temp;
         }
-
-
     }
+
+
 }

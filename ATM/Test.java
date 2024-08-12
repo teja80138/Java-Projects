02c2d15aa7884ff7f37f057ce3b7c4515b4e127e
NeilB18 <82923875+NeilB18@users.noreply.github.com>
2024-08-12 09:54:02 -0700

    reboot

--- ATM/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test{
-    public static void main(String[] args){
-        String[] words = {"ten", "fading", "post", "card", "thunder", "hinge", "trailing", "batting"};
-        for (String i : words){
-            if (i.substring(i.length()-3,i.length()).equals("ing"))
-                System.out.println(i);
-        }
-    }
-}
--- CodeGen/Main.java
+++
@@ -1,8 +0,0 @@
-package CodeGen;
-
-public class Main {
-    public static void main(String[] args) {
-        String s = "g";
-        System.out.println(s.substring(1));
-    }
-}
